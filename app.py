8301015b34e5cacc255e6c7a295594e77cee71f2
Ebipatei <ebitunyan@gmail.com>
2025-02-03 00:29:19 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, jsonify
+import requests
+import math
+
+app = Flask(__name__)
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def is_perfect(n):
+    if n < 2:
+        return False
+    sum_divisors = 1
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            sum_divisors += i
+            if i != n // i:
+                sum_divisors += n // i
+    return sum_divisors == n
+
+def is_armstrong(n):
+    digits = [int(d) for d in str(n)]
+    num_digits = len(digits)
+    return n == sum(d ** num_digits for d in digits)
+
+def digit_sum(n):
+    return sum(int(d) for d in str(n))
+
+def get_fun_fact(n):
+    response = requests.get(f"http://numbersapi.com/{n}/math?json")
+    if response.status_code == 200:
+        return response.json().get('text', 'No fun fact available.')
+    return 'No fun fact available.'
+
+@app.route('/api/classify-number', methods=['GET'])
+def classify_number():
+    number = request.args.get('number', type=int)
+    if number is None:
+        return jsonify({"number": request.args.get('number'), "error": True}), 400
+
+    properties = []
+    if is_armstrong(number):
+        properties.append("armstrong")
+    if number % 2 == 0:
+        properties.append("even")
+    else:
+        properties.append("odd")
+
+    response = {
+        "number": number,
+        "is_prime": is_prime(number),
+        "is_perfect": is_perfect(number),
+        "properties": properties,
+        "digit_sum": digit_sum(number),
+        "fun_fact": get_fun_fact(number)
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
