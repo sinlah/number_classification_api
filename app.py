8c56040d93ff4df3bbbb8d22eb324e4edb96e357
Ebipatei <ebitunyan@gmail.com>
2025-02-03 17:45:10 -0500

    Use OrderedDict to fix JSON response order

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import math
 from flask_cors import CORS
 import os
+from collections import OrderedDict
 
 app = Flask(__name__)
 CORS(app)
@@ -63,14 +64,24 @@ def classify_number():
     else:
         properties.append("odd")
 
-    response = {
-        "number": number,
-        "is_prime": is_prime(number),
-        "is_perfect": is_perfect(number),
-        "properties": properties,
-        "digit_sum": digit_sum(number),
-        "fun_fact": get_fun_fact(number)
-    }
+    #use OrderedDict to preserve the exact key order
+    response = OrderedDict([
+        ("number", number),
+        ("is_prime", is_prime(number)),
+        ("is_perfect", is_perfect(number)),
+        ("properties", properties),
+        ("digit_sum", digit_sum(number)),
+        ("fun_fact", get_fun_fact(number))
+    ])
+
+    # response = {
+    #     "number": number,
+    #     "is_prime": is_prime(number),
+    #     "is_perfect": is_perfect(number),
+    #     "properties": properties,
+    #     "digit_sum": digit_sum(number),
+    #     "fun_fact": get_fun_fact(number)
+    # }
     return jsonify(response), 200
 
 if __name__ == '__main__':
